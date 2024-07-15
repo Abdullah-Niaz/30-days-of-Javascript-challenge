f3a7a6268c24015f6efed012978697e6755f305c
Abdullah-Niaz <abdullahniaz423@gmail.com>
2024-07-15 17:21:52 +0500

    Variable Declaration

--- 01_Day/01_Task.js
+++ 01_Day/01_Task.js
@@ -1,5 +1,7 @@
+// create a variable with var and assign it a number
 var num  = 20;
 console.log(num)
 
+// create a variable using let and assign it a string
 let name = "Abdullah";
 console.log(name)
