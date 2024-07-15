a07f19bd97bcebe87223e536757fe6c94f642c09
Abdullah-Niaz <abdullahniaz423@gmail.com>
2024-07-15 17:26:03 +0500

    data types

---
+++ 01_Day/03_Task.js
@@ -0,0 +1,20 @@
+const n = Number;
+const s= String;
+const b = Boolean;
+const o = Object;
+const a = Array;
+
+console.log(n);
+console.log(typeof n)
+
+console.log(s);
+console.log(typeof s)
+
+console.log(b);
+console.log(typeof b)
+
+console.log(o);
+console.log(typeof o)
+
+console.log(o);
+console.log(typeof o)
