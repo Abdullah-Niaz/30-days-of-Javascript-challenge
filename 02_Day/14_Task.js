0b45c9ad3c5b2ce922721edab0b2bae34651aeb3
Abdullah-Niaz <abdullahniaz423@gmail.com>
2024-07-17 17:12:58 +0500

    Ternary  Operator

---
+++ 02_Day/14_Task.js
@@ -0,0 +1,8 @@
+const value1 = 60;
+const value2 = 40;
+const value3 = 60;
+const value4 = 60;
+
+// Using the ternary operator to evaluate a condition
+var result = (value1 + value2) > 50 ? (value3 + value4) : (value1 + value2);
+console.log(result);
