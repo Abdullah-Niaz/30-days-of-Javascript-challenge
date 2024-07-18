e0740191a74481bb8ff6eff9ee72dfe8d2ebf056
Abdullah-Niaz <abdullahniaz423@gmail.com>
2024-07-18 17:40:20 +0500

    check if the person is eligible for vote or not

---
+++ 03_Day/02_Task.js
@@ -0,0 +1,9 @@
+// Write a Program to check if the person is eligible for vote or not
+var personAge  = 20;
+
+if(personAge >= 18){
+    console.log("You are eligible for vote");
+}
+else{
+    console.log("You are not eligible for vote");
+}
