ba2cfe0a87774e56ce0ba400d0ef1a4233cbe98f
Abdullah-Niaz <abdullahniaz423@gmail.com>
2024-07-18 17:38:19 +0500

    given number is positive, negative or zero

---
+++ 03_Day/01_Task.js
@@ -0,0 +1,12 @@
+// Write a program to check if the given number is positive or negative or zero and log the result 
+var n = 0;
+
+if(n == 0 ){
+    console.log("Number is Zero");
+}
+else if( n < 0 ){
+    console.log("Number is Negative");
+}
+else{
+    console.log("Number is Positive");
+}
