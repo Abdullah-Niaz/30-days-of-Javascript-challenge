ad72e07ef1014fc77788d549dab9b60f04645c0b
Abdullah-Niaz <abdullahniaz423@gmail.com>
2024-07-18 17:42:05 +0500

    find the largest of three number

---
+++ 03_Day/03_Task.js
@@ -0,0 +1,16 @@
+// Write a program to find the largest of three number
+var a = 10;
+var b = 20;
+var c = 30;
+
+if(a > b){
+    if(a > c){
+        console.log("A is greater : ", a);
+    }
+}
+else if( b > c){
+    console.log("B is greater : ", b);
+}
+else{
+    console.log("C is greater : ", c);
+}
