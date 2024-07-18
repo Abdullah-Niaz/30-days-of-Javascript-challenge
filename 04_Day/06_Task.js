64ce40d0fed7732ddca1b8c0dc459bca4f186f40
Abdullah-Niaz <abdullahniaz423@gmail.com>
2024-07-18 22:16:12 +0500

    Factorial of n using while loop

---
+++ 04_Day/06_Task.js
@@ -0,0 +1,10 @@
+// write a program to print he factorial of number using do while loop
+
+let n = 10;
+let fact = 1;
+let i = 1;
+while(i<=n){
+    fact *= i;
+    i++;
+}
+console.log("Factorial of 10 is: ", fact);
