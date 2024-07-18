d16210a4e9eb442a1124cebde1248b6adb525efd
Abdullah-Niaz <abdullahniaz423@gmail.com>
2024-07-18 17:45:24 +0500

    switch case statements to dertermine the day of week based on the number

---
+++ 03_Day/04_Task.js
@@ -0,0 +1,30 @@
+// write a program that uses switch case statements to dertermine the day of week based on the number
+
+var d = 1;
+
+switch (d) {
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("Tuesday");
+        break;
+    case 3:
+        console.log("Wednesday");
+        break;
+    case 4:
+        console.log("Thursday");
+        break;
+    case 5:
+        console.log("Friday");
+        break;
+    case 6:
+        console.log("Saturday");
+        break;
+    case 7:
+        console.log("Sunday");
+        break;
+    default:
+        console.log("Invalid day");
+        break;
+}
