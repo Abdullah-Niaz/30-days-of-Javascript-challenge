5dd072487b349fbb6c1d19c54a5e06b00056225f
Abdullah-Niaz <abdullahniaz423@gmail.com>
2024-07-21 00:00:01 +0500

    maxOfTwo

---
+++ 05_Day/03_Task.js
@@ -0,0 +1,12 @@
+// write a function to find the max of two 
+
+function maxOfTwo(a,b){
+    if(a>b){
+        return a;
+    }
+    else{
+        return b;
+    }
+}
+m = maxOfTwo(2,4);
+console.log("Max No : ", m)
