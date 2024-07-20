238bd891dfab08f81ddecd2d9813c1fdca693cf6
Abdullah-Niaz <abdullahniaz423@gmail.com>
2024-07-20 23:56:28 +0500

    even odd checker

---
+++ 05_Day/01_Task.js
@@ -0,0 +1,12 @@
+// wrte a function to check if the given number is even or odd 
+
+function Nchecker(n){
+    if(n%2==0){
+        return "Even";
+    }
+    else{
+        return "ODD";
+    }
+}
+n = Nchecker(20);
+console.log(n);
