06848deb4061e8f901eb18c461a0f487c5991f66
Abdullah-Niaz <abdullahniaz423@gmail.com>
2024-07-22 00:18:02 +0500

    object Destructing

--- 06_Day/01_Task.js
+++ 06_Day/01_Task.js
@@ -3,3 +3,15 @@
 const arr = [1, 2, 3, 4, 5];
 
 console.log(...arr)
+
+const arr1 = [1, 2, 3];
+const arr2 = [4, 5, 6];
+const combined = [...arr1, ...arr2];
+console.log(combined);
+
+
+
+const [first, second, ...rest] = arr;
+console.log(first);   // 1
+console.log(second);  // 2
+console.log(rest);    // [3, 4, 5]
